a90176ea8a7c46963247a84b1a47963eb342ada5
Arjun <arjunp4dev@gmail.com>
2026-02-11 03:55:15 +0530

    Configured frontend routing

--- taskmanager/config/urls.py
+++ taskmanager/config/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "frontend/login.html")
 
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-
-    # Frontend Pages
-    path("", TemplateView.as_view(template_name="frontend/login.html")),
-    path("login.html", TemplateView.as_view(template_name="frontend/login.html")),
-    path("register.html", TemplateView.as_view(template_name="frontend/register.html")),
-    path("projects.html", TemplateView.as_view(template_name="frontend/projects.html")),
-    path("board.html", TemplateView.as_view(template_name="frontend/board.html")),
 ]
