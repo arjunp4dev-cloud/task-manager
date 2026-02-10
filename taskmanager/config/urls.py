8c69a3b42e38324ffa87d936abd9bbe64ad9f357
Arjun <arjunp4dev@gmail.com>
2026-02-11 01:32:48 +0530

    Fixed urls import issue

--- taskmanager/config/urls.py
+++ taskmanager/config/urls.py
@@ -1,7 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
 from django.http import JsonResponse
+
 
 def home(request):
     return JsonResponse({"message": "Task Manager API Running"})
+
 
 urlpatterns = [
     path("", home),
